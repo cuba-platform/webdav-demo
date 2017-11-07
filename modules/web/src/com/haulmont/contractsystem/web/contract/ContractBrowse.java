53304ac19e07ed29ad248b1faa30c50f3c3361a3
Evgeniy Zharenov <zharenov@haulmont.com>
2017-11-07 19:45:58 +0400

    WD-408 Preparing for demo

--- modules/web/src/com/haulmont/contractsystem/web/contract/ContractBrowse.java
+++ modules/web/src/com/haulmont/contractsystem/web/contract/ContractBrowse.java
@@ -3,11 +3,8 @@ package com.haulmont.contractsystem.web.contract;
 import com.haulmont.contractsystem.entity.Contract;
 import com.haulmont.cuba.gui.components.AbstractLookup;
 import com.haulmont.cuba.gui.components.Component;
-import com.haulmont.cuba.gui.components.Table;
 import com.haulmont.cuba.gui.xml.layout.ComponentsFactory;
 import com.haulmont.webdav.components.DocumentLink;
-import com.haulmont.webdav.entity.WebdavDocumentVersion;
-import com.haulmont.webdav.service.WebdavDocumentVersionsManagementService;
 
 import javax.inject.Inject;
 
@@ -16,18 +13,8 @@ public class ContractBrowse extends AbstractLookup {
     @Inject
     protected ComponentsFactory componentsFactory;
 
-    @Inject
-    protected WebdavDocumentVersionsManagementService documentVersionsService;
-
     public Component generateLinkCell(Contract entity) {
         return componentsFactory.createComponent(DocumentLink.class)
                 .withFileDescriptor(entity.getDocument());
-    }
-
-    public Component generateLatestVersionCell(Contract entity) {
-        WebdavDocumentVersion latestVersion = documentVersionsService.retrieveLastVersionByFileDescriptorNN(
-                entity.getDocument(), "webdavDocumentVersion-with-deps-view");
-
-        return new Table.PlainTextCell(String.valueOf(latestVersion.getNaturalVersionId()));
     }
 }
