27475204cfc5a1404f3d4fd4c992edb4e2f367cf
Evgeniy Zharenov <zharenov@haulmont.com>
2017-11-22 18:20:39 +0400

    WD-479 Add WebDavDocument browse for administration menu with button for removing exclusive lock

--- modules/global/src/com/haulmont/contractsystem/entity/Contract.java
+++ modules/global/src/com/haulmont/contractsystem/entity/Contract.java
@@ -4,30 +4,26 @@ import com.haulmont.cuba.core.entity.FileDescriptor;
 import com.haulmont.cuba.core.entity.StandardEntity;
 import com.haulmont.cuba.core.entity.annotation.OnDelete;
 import com.haulmont.cuba.core.global.DeletePolicy;
+import com.haulmont.webdav.annotation.WebdavSupport;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-import java.util.List;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-import com.haulmont.cuba.core.entity.annotation.Listeners;
 
 @Table(name = "CONTRACTSYSTEM_CONTRACT")
 @Entity(name = "contractsystem$Contract")
 public class Contract extends StandardEntity {
-    private static final long serialVersionUID = -1834442845605423406L;
+    private static final long serialVersionUID = - 1834442845605423406L;
 
     @Column(name = "NAME", nullable = false)
     protected String name;
 
-
     @NotNull
+    @WebdavSupport
     @OnDelete(DeletePolicy.CASCADE)
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "DOCUMENT_ID")
@@ -54,8 +50,6 @@ public class Contract extends StandardEntity {
     public FileDescriptor getDocument() {
         return document;
     }
-
-
 
 
     public void setName(String name) {
