2912b24636840b858fe7a27e9df4042aa9a4a15a
Evgeniy Zharenov <zharenov@haulmont.com>
2017-12-20 13:26:45 +0400

    WD-286 Implement instrument to remove all documents which haven't related entities

--- modules/global/src/com/haulmont/contractsystem/entity/ContractUser.java
+++ modules/global/src/com/haulmont/contractsystem/entity/ContractUser.java
@@ -1,33 +1,18 @@
 package com.haulmont.contractsystem.entity;
 
-import javax.persistence.Embedded;
-import javax.persistence.Entity;
 import com.haulmont.cuba.core.entity.annotation.Extends;
-import javax.persistence.Column;
 import com.haulmont.cuba.security.entity.User;
 import com.haulmont.webdav.entity.WebdavCredentials;
-import com.haulmont.webdav.entity.WebdavUserEmbedded;
-import com.haulmont.webdav.entity.WebdavUserImplemented;
-import java.util.List;
+
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Extends(User.class)
 @Entity(name = "contractsystem$ContractUser")
-public class ContractUser extends User implements WebdavUserImplemented {
+public class ContractUser extends User {
     private static final long serialVersionUID = 3199627093188083975L;
 
     @OneToOne(mappedBy = "user", fetch = FetchType.LAZY)
     protected WebdavCredentials webdavCredentials;
-
-    @Override
-    public WebdavCredentials getWebdavCredentials() {
-        return webdavCredentials;
-    }
-
-    @Override
-    public void setWebdavCredentials(WebdavCredentials webdavCredentials) {
-        this.webdavCredentials = webdavCredentials;
-    }
 }
