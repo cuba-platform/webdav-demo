d870d424aa401cd42e3a6e3bc003d76881a5cdd8
Ilia Kuchmin <kuchmin@haulmont.com>
2018-03-30 18:56:07 +0400

    WD-654 IllegalStateException при попытке сохранения контракта у еще не сохраненного контрактора

--- modules/web/src/com/haulmont/contractsystem/web/contractor/ContractorEdit.java
+++ modules/web/src/com/haulmont/contractsystem/web/contractor/ContractorEdit.java
@@ -2,12 +2,6 @@ package com.haulmont.contractsystem.web.contractor;
 
 import com.haulmont.cuba.gui.components.AbstractEditor;
 import com.haulmont.contractsystem.entity.Contractor;
-import com.haulmont.cuba.gui.components.Component;
-import com.haulmont.contractsystem.entity.Contract;
 
 public class ContractorEdit extends AbstractEditor<Contractor> {
-
-    public Component generateDocumentCell(Contract entity) {
-		return null;
-    }
 }
