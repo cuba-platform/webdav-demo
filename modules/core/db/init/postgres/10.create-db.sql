5d3f826e86bb20652618ac0f69879219f864e959
zaharchenko <zaharchenko@haulmont.com>
2019-09-16 16:57:42 +0400

    fix postgres scripts

--- modules/core/db/init/postgres/10.create-db.sql
+++ modules/core/db/init/postgres/10.create-db.sql
@@ -1,98 +1,10 @@
--- begin WEBDAV_DEMO_CONTRACT
-create table WEBDAV_DEMO_CONTRACT (
-    ID uuid,
-    VERSION integer not null,
-    CREATE_TS timestamp,
-    CREATED_BY varchar(50),
-    UPDATE_TS timestamp,
-    UPDATED_BY varchar(50),
-    DELETE_TS timestamp,
-    DELETED_BY varchar(50),
-    --
-    NAME varchar(255) not null,
-    DOCUMENT_ID uuid not null,
-    CONTRACTOR_ID uuid,
-    --
-    primary key (ID)
-)^
--- end WEBDAV_DEMO_CONTRACT
--- begin WEBDAV_DEMO_CONTRACTOR
-create table WEBDAV_DEMO_CONTRACTOR (
-    ID uuid,
-    VERSION integer not null,
-    CREATE_TS timestamp,
-    CREATED_BY varchar(50),
-    UPDATE_TS timestamp,
-    UPDATED_BY varchar(50),
-    DELETE_TS timestamp,
-    DELETED_BY varchar(50),
-    --
-    NAME varchar(255) not null,
-    EMAIL varchar(255) not null,
-    --
-    primary key (ID)
-)^
--- end WEBDAV_DEMO_CONTRACTOR
--- begin WEBDAV_DEMO_WEBDAV_CONTRACT
-create table WEBDAV_DEMO_WEBDAV_CONTRACT (
-    ID uuid,
-    VERSION integer not null,
-    CREATE_TS timestamp,
-    CREATED_BY varchar(50),
-    UPDATE_TS timestamp,
-    UPDATED_BY varchar(50),
-    DELETE_TS timestamp,
-    DELETED_BY varchar(50),
-    --
-    NAME varchar(255),
-    CONTRACTOR_ID uuid,
-    DOCUMENT_ID uuid,
-    --
-    primary key (ID)
-)^
--- end WEBDAV_DEMO_WEBDAV_CONTRACT
--- begin WEBDAV_DEMO_CONTRACT_WITH_DISABLED_WEBDAV
-create table WEBDAV_DEMO_CONTRACT_WITH_DISABLED_WEBDAV (
-    ID uuid,
-    VERSION integer not null,
-    CREATE_TS timestamp,
-    CREATED_BY varchar(50),
-    UPDATE_TS timestamp,
-    UPDATED_BY varchar(50),
-    DELETE_TS timestamp,
-    DELETED_BY varchar(50),
-    --
-    NAME varchar(255),
-    FILE_ID uuid,
-    --
-    primary key (ID)
-)^
--- end WEBDAV_DEMO_CONTRACT_WITH_DISABLED_WEBDAV
--- begin WEBDAV_DEMO_CONTRACT_WITH_SIMPLE_FILE_DESCRIPTOR
-create table WEBDAV_DEMO_CONTRACT_WITH_SIMPLE_FILE_DESCRIPTOR (
-    ID uuid,
-    VERSION integer not null,
-    CREATE_TS timestamp,
-    CREATED_BY varchar(50),
-    UPDATE_TS timestamp,
-    UPDATED_BY varchar(50),
-    DELETE_TS timestamp,
-    DELETED_BY varchar(50),
-    --
-    NAME varchar(255),
-    FILE_ID uuid,
-    --
-    primary key (ID)
-)^
--- end WEBDAV_DEMO_CONTRACT_WITH_SIMPLE_FILE_DESCRIPTOR
-
 -- begin SEC_USER
 alter table SEC_USER add column DTYPE varchar(100) ^
 update SEC_USER set DTYPE = 'webdavdemo$ContractUser' where DTYPE is null ^
 -- end SEC_USER
 
--- begin WEBDAV_DEMO_ENTITY_MANY_FD
-create table WEBDAV_DEMO_ENTITY_MANY_FD (
+-- begin WEBDAV_DEMO_CONTRACT_WD_WITH_ENABLED_VERSIONING
+create table WEBDAV_DEMO_CONTRACT_WD_WITH_ENABLED_VERSIONING (
     ID uuid,
     VERSION integer not null,
     CREATE_TS timestamp,
@@ -102,15 +14,118 @@ create table WEBDAV_DEMO_ENTITY_MANY_FD (
     DELETE_TS timestamp,
     DELETED_BY varchar(50),
     --
-    NAME varchar(255),
+    NUMBER_ varchar(255) not null,
+    CONTRACTOR_ID uuid not null,
+    DOCUMENT_ID uuid not null,
     --
     primary key (ID)
 )^
--- end WEBDAV_DEMO_ENTITY_MANY_FD
--- begin WEBDAV_DEMO_MANY
-create table WEBDAV_DEMO_MANY (
-    ENTITY_MANY_ID uuid,
-    FILE_DESCRIPTOR_ID uuid,
-    primary key (ENTITY_MANY_ID, FILE_DESCRIPTOR_ID)
+-- end WEBDAV_DEMO_CONTRACT_WD_WITH_ENABLED_VERSIONING
+-- begin WEBDAV_DEMO_CONTRACT_FD_WITH_ENABLED_WEBDAV_ENABLED_VERSIONING
+create table WEBDAV_DEMO_CONTRACT_FD_WITH_ENABLED_WEBDAV_ENABLED_VERSIONING (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    NUMBER_ varchar(255) not null,
+    CONTRACTOR_ID uuid not null,
+    DOCUMENT_ID uuid,
+    --
+    primary key (ID)
 )^
--- end WEBDAV_DEMO_MANY
+-- end WEBDAV_DEMO_CONTRACT_FD_WITH_ENABLED_WEBDAV_ENABLED_VERSIONING
+-- begin WEBDAV_DEMO_CONTRACT_FD_WITH_DEFAULT_BEHAVIOUR
+create table WEBDAV_DEMO_CONTRACT_FD_WITH_DEFAULT_BEHAVIOUR (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    NUMBER_ varchar(255) not null,
+    CONTRACTOR_ID uuid not null,
+    DOCUMENT_ID uuid not null,
+    --
+    primary key (ID)
+)^
+-- end WEBDAV_DEMO_CONTRACT_FD_WITH_DEFAULT_BEHAVIOUR
+-- begin WEBDAV_DEMO_CONTRACT_WD_WITH_DISABLED_VERSIONING
+create table WEBDAV_DEMO_CONTRACT_WD_WITH_DISABLED_VERSIONING (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    NUMBER_ varchar(255) not null,
+    CONTRACTOR_ID uuid not null,
+    DOCUMENT_ID uuid not null,
+    --
+    primary key (ID)
+)^
+-- end WEBDAV_DEMO_CONTRACT_WD_WITH_DISABLED_VERSIONING
+-- begin WEBDAV_DEMO_CONTRACT_FD_WITH_ENABLED_WEBDAV_DISABLED_VERSIONING
+create table WEBDAV_DEMO_CONTRACT_FD_WITH_ENABLED_WEBDAV_DISABLED_VERSIONING (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    NUMBER_ varchar(255) not null,
+    CONTRACTOR_ID uuid not null,
+    DOCUMENT_ID uuid not null,
+    --
+    primary key (ID)
+)^
+-- end WEBDAV_DEMO_CONTRACT_FD_WITH_ENABLED_WEBDAV_DISABLED_VERSIONING
+-- begin WEBDAV_DEMO_CONTRACT_FD_WITH_DISABLED_WEBDAV
+create table WEBDAV_DEMO_CONTRACT_FD_WITH_DISABLED_WEBDAV (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    NUMBER_ varchar(255) not null,
+    CONTRACTOR_ID uuid not null,
+    DOCUMENT_ID uuid not null,
+    --
+    primary key (ID)
+)^
+-- end WEBDAV_DEMO_CONTRACT_FD_WITH_DISABLED_WEBDAV
+-- begin WEBDAV_DEMO_CONTRACT_WD_WITH_DEFAULT_BEHAVIOUR
+create table WEBDAV_DEMO_CONTRACT_WD_WITH_DEFAULT_BEHAVIOUR (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    NUMBER_ varchar(255) not null,
+    CONTRACTOR_ID uuid not null,
+    DOCUMENT_ID uuid not null,
+    --
+    primary key (ID)
+)^
+-- end WEBDAV_DEMO_CONTRACT_WD_WITH_DEFAULT_BEHAVIOUR
